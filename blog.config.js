667fc25f5ad98454be60e855c420c7655e3edb49
FLYPEGION <76755437+FLYPEGION@users.noreply.github.com>
2023-07-29 00:43:33 +0800

    Update introduce

--- blog.config.js
+++ blog.config.js
@@ -13,14 +13,14 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
-  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || '关于独立产品的一切, 从独立产品到小公司',
+  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || '独立产品起步, 从独立产品到小公司',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'FLYPEGION', // 您的昵称
-  BIO: process.env.NEXT_PUBLIC_BIO || '摸索独立应用中', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '探索独立应用中', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.indiehub.cn', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '独立产品, AI产品, 数字游民, 独立开发者, 产品灵感, 产品推广, 产品变现, indiehub', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '独立产品, 数字游民, 独立开发者, 产品灵感, 产品推广, 产品变现, indiehub', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
